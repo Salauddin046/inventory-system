205f1311d38eba491791e75623b8170bcc92239b
salauddin046 <kssalauddin@intellicar.in>
2026-05-21 18:54:04 +0530

    update

--- app/api/projection/route.ts
+++ app/api/projection/route.ts
@@ -138,28 +138,4 @@ export async function POST(
 
   }
 
-}{ NextResponse } from "next/server";
-import sql from "@/lib/db";
-
-export async function GET() {
-
-  try {
-
-    const data =
-      await sql`
-        SELECT *
-        FROM projection_master
-        ORDER BY id DESC
-      `;
-
-    return NextResponse.json(data);
-
-  } catch (error) {
-
-    console.log(error);
-
-    return NextResponse.json([]);
-
-  }
-
 }
