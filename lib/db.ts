e226cc2fca2ae1f7eae69c12bc5e0a40b02f79bf
salauddin046 <kssalauddin@intellicar.in>
2026-05-21 16:40:47 +0530

    Update

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,10 @@
 import postgres from "postgres";
 
-const sql = postgres(process.env.DATABASE_URL!);
+const sql = postgres(
+  process.env.DATABASE_URL!,
+  {
+    ssl: "require"
+  }
+);
 
 export default sql;
