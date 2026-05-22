4014a7d6b466a9a7e4adb61365313886b822cb99
salauddin046 <kssalauddin@intellicar.in>
2026-05-22 18:40:34 +0530

    Update

--- app/api/projection/route.ts
+++ app/api/projection/route.ts
@@ -1,6 +1,3 @@
-import { NextResponse } from "next/server";
-import sql from "@/lib/db";
-
 export async function GET() {
 
   try {
@@ -10,7 +7,7 @@ export async function GET() {
 
         SELECT *
 
-        FROM projection_master
+        FROM projection_upload
 
         ORDER BY id DESC
 
@@ -20,110 +17,11 @@ export async function GET() {
       data
     );
 
-  } catch (error: any) {
+  } catch (error) {
 
     console.log(error);
 
     return NextResponse.json([]);
-
-  }
-
-}
-
-export async function PUT(
-  request: Request
-) {
-
-  try {
-
-    const body =
-      await request.json();
-
-    await sql`
-
-      UPDATE projection_master
-
-      SET
-
-        projection_action =
-        COALESCE(
-          ${body.projection_action},
-          projection_action
-        ),
-
-        stock_action =
-        COALESCE(
-          ${body.stock_action},
-          stock_action
-        ),
-
-        stock_qty =
-        COALESCE(
-          ${Number(
-            body.stock_qty || 0
-          )},
-          stock_qty
-        )
-
-      WHERE id =
-      ${body.id}
-
-    `;
-
-    return NextResponse.json({
-
-      success: true
-
-    });
-
-  } catch (error: any) {
-
-    console.log(error);
-
-    return NextResponse.json({
-
-      success: false
-
-    });
-
-  }
-
-}
-
-export async function DELETE(
-  request: Request
-) {
-
-  try {
-
-    const body =
-      await request.json();
-
-    await sql`
-
-      DELETE FROM
-      projection_master
-
-      WHERE id =
-      ${body.id}
-
-    `;
-
-    return NextResponse.json({
-
-      success: true
-
-    });
-
-  } catch (error: any) {
-
-    console.log(error);
-
-    return NextResponse.json({
-
-      success: false
-
-    });
 
   }
 
